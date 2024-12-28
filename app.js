adbf4e81c4df0ce35ea0a028c33044cfa6c59629
Ramdev <work.ramdev@gmail.com>
2024-12-28 12:16:08 +0530

    minor changes about static dynamic email generated

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   // throw new Error('Sync Dummy');
-  if (!req.session.user) {
+  if (!req.session?.user) {
     return next();
   }
   User.findById(req.session.user._id)
